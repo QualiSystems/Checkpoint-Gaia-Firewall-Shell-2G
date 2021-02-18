560af58a6ff6ad36d74f48ac7a78aab46cb6b1ab
Yaroslav Nikonorov <yaroslav.n@quali.com>
2021-02-18 18:11:08 +0200

    Added simple unittest

---
+++ tests/test_checkpoin_driver.py
@@ -0,0 +1,11 @@
+import unittest
+
+from driver import CheckPointGaiaFirewallShell2GDriver
+
+
+class TestCheckPointGaiaFirewallShell2GDriver(unittest.TestCase):
+    def setUp(self):
+        self._instance = CheckPointGaiaFirewallShell2GDriver()
+
+    def test_init(self):
+        self.assertIsNone(self._instance._cli)
